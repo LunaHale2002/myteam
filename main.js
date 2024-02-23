bd9941a3c730a394d3d58b93bb1a24945f09c1ba
LunaHale2002 <lbabic@edenschool.fr>
2024-02-23 16:47:32 +0100

    home

---
+++ main.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import "./about.css";
+import "./contact.css";
+
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+const closeBtn = document.querySelector("#close-btn");
+
+burger.addEventListener("click", () => {
+  menu.classList.add("open");
+});
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
